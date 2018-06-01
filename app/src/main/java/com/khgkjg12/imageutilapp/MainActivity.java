5d1f7c81da9d316a082624d42daabeae57229e6b
Kang Hyungu <khgkjg12@naver.com>
2018-06-01 19:07:17 +0900

    테스트 앱 시작

--- app/src/main/java/com/khgkjg12/imageutilapp/MainActivity.java
+++ app/src/main/java/com/khgkjg12/imageutilapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.khgkjg12.imageutilapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView testImage = findViewById(R.id.test_image);
+
     }
 }
